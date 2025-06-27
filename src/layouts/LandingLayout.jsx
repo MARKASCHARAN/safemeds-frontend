27ebbd138c61a8589f6777ffd2165586c80e99a3
MARKASCHARAN <markascharan@gmail.com>
2025-06-28 01:14:35 +0530

    first commit

---
+++ src/layouts/LandingLayout.jsx
@@ -0,0 +1,22 @@
+import HeroSection from '../components/HeroSection';
+import Navbar from '../components/Navbar';
+import FeatureCards from '../components/FeatureCards';
+import HowToUse from '../components/HowToUse';
+import Footer from '../components/Footer';
+
+const LandingLayout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen ">
+
+      <Navbar />
+      <HeroSection />
+      <FeatureCards />
+      <HowToUse />
+      <Footer />  
+    
+      <main className="flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default LandingLayout;
